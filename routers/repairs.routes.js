f7ab6e7a6cce5346a0fd5d2214ca28773a784e2b
jaquelineClaudia <100646805+jaquelineClaudia@users.noreply.github.com>
2022-05-17 19:56:43 -0400

    computer-repairs-app

---
+++ routers/repairs.routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { pendingRepairExists } = require('../middlewares/repairs.middlewares');
+const {
+  createRepairValidations,
+  checkValidations,
+} = require('../middlewares/validators.middleware');
+
+const {
+  getAllCompletedRepairs,
+  getAllPendingRepairs,
+  createRepair,
+  getRepairById,
+  repairCancelled,
+  repairCompleted,
+} = require('../controllers/repairs.controller');
+
+const router = express.Router();
+
+router.get('/completed', getAllCompletedRepairs);
+
+router.get('/pending', getAllPendingRepairs);
+
+router.post('/', createRepairValidations, checkValidations, createRepair);
+
+router.get('/:id', pendingRepairExists, getRepairById);
+
+router.patch('/:id', pendingRepairExists, repairCompleted);
+
+router.delete('/:id', pendingRepairExists, repairCancelled);
+
+module.exports = { repairsRouter: router };
---
+++ server.js
@@ -0,0 +1,21 @@
+const { app } = require('./app');
+const { User } = require('./models/user.model');
+const { Repair } = require('./models/repair.model');
+const { db } = require('./utils/database');
+
+db.authenticate()
+  .then(() => console.log('Database authenticated'))
+  .catch((err) => console.log(err));
+
+User.hasMany(Repair);
+Repair.belongsTo(User);
+
+db.sync()
+  .then(() => console.log('Database synced'))
+  .catch((err) => console.log(err));
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Express app running on port ${PORT}`);
+});
